5ca92473432642899bc7ba769dc33f0025f21178
Wesley Steve <wesley.silva23@hotmail.com>
2022-07-18 19:18:52 -0300

    feat: python/load/load.py adicionado a funcao data_load responsavel em armazenar os dados em um banco de dados sqlite coletados com a funcao data_collect do arquivo python/extract/collect.py

---
+++ estrutura_projeto_dados/nome_projeto/python/load/load.py
@@ -0,0 +1,29 @@
+import os
+
+import pandas as pd
+
+from bancoslib.db.sqlite import create_db_sqlite
+
+
+def data_load(data_raw, raw_dir, banco_dir=None):
+    """Função responsavel em carregar os dados no banco de dados"""
+
+    if banco_dir == "":
+
+        con = create_db_sqlite(raw_dir)
+
+    con = create_db_sqlite(banco_dir)
+
+    if con:
+
+        for i in data_raw:
+            df_tmp = pd.read_csv(os.path.join(raw_dir, i))
+            # coloque a funcao abaixo na frente do ) caso queira remover
+            # alguma parte no nome do arquivo
+            # ex: .replace("olist_", "")
+            nome_tabela = i.strip(".csv")
+            df_tmp.to_sql(nome_tabela, con, if_exists="replace", index=False)
+
+        return True
+
+    return False
